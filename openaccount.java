c6ab6db4da23c2dbb69ef46a69744391171959f2
Shulabh-968026 <58615131+Shulabh-968026@users.noreply.github.com>
2020-03-22 16:22:28 +0530

    Update openaccount.java

--- openaccount.java
+++ openaccount.java
@@ -84,53 +84,7 @@ public class openaccount extends  HttpServlet{
 	        {
 	           System.out.println("sql exception::"+e.getMessage());
 	        }
-		 // mailer.send();
-		/*  finally
-		  {
-			  try
-			  {
-				  rs.close();
-				  st.close();
-			  }
-			  catch(SQLException e)
-			  {
-				  out.println(e);
-			  }
-		  }*/  
-			/*  public static void send(String to)
-			  {
-			  final String user="shulabhdixit143@gmail.com";//change accordingly  
-			  final String pass="ankita9680";  
-			  final String subject="Bank";
-			  final String msg="your account success fully open";
-			    
-			  //1st step) Get the session object    
-			  Properties props = new Properties();  
-			  props.put("mail.smtp.host", "gmail.com");//change accordingly  
-			  props.put("mail.smtp.auth", "true");  
-			    
-			  Session session = Session.getDefaultInstance(props,  
-			   new javax.mail.Authenticator() {  
-			    protected PasswordAuthentication getPasswordAuthentication() {  
-			     return new PasswordAuthentication(user,pass);  
-			     }  
-			  });  
-			  //2nd step)compose message  
-			  try {  
-			   MimeMessage message = new MimeMessage(session);  
-			   message.setFrom(new InternetAddress(user));  
-			   message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
-			   message.setSubject(subject);  
-			   message.setText(msg);  
-			     
-			   //3rd step)send message  
-			   Transport.send(message);  
-			    
-			   System.out.println("Done");  
-			    
-			   } catch (MessagingException e) {  
-			      throw new RuntimeException(e);  
-			   }*/  
+		 
 	}
 
 }
